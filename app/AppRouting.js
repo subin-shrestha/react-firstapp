c5156c9783c6e8ce2ac08abe11a159c7fe49b34f
Subin Shrestha <developer.shrestha@gmail.com>
2020-09-16 19:34:52 +0545

    update routing

---
+++ app/AppRouting.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react"
+import { Switch, Route } from "react-router-dom"
+import UserContext from "./_contexts/UserContext"
+import About from "./components/footer/About"
+import Terms from "./components/footer/Terms"
+import Home from "./components/home/Home"
+import HomeGuest from "./components/home/HomeGuest"
+
+function AppRouting(props) {
+  const { loggedIn } = useContext(UserContext)
+
+  return (
+    <Switch>
+      <Route path="/" exact>
+        {loggedIn ? <Home /> : <HomeGuest />}
+      </Route>
+      <Route path="/about-us">
+        <About />
+      </Route>
+      <Route path="/terms">
+        <Terms />
+      </Route>
+    </Switch>
+  )
+}
+
+export default AppRouting
